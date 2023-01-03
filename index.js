518f9e49a51192c3d7d504a80c73dac034e4de0c
kei-nishikawa <xnkcelebrate48x@yahoo.co.jp>
2023-01-03 16:47:50 +0900

    fix memberLimitSet

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const {
   ChannelType,
   GatewayIntentBits,
   Collection,
-} = require("discord.js");
-const { serverInfo } = require("./lib/serverInfo.js");
-const { showComponent, replayChampionInfoURL } = require("./lib/utils.js");
+} = require('discord.js')
+const { serverInfo } = require('./lib/serverInfo.js')
+const { showComponent, replayChampionInfoURL } = require('./lib/utils.js')
 const {
   CreateRoomButtonBuilder,
   GumaButtonsBuilder,
@@ -18,11 +18,11 @@ const {
   ChampionMatchUpSearchModalBuilder,
   JumpPointBuilder,
   OpggModalBuilder,
-} = require("./lib/customBuilder.js");
-const fs = require("node:fs");
-const path = require("node:path");
-const dotenv = require("dotenv");
-dotenv.config();
+} = require('./lib/customBuilder.js')
+const fs = require('node:fs')
+const path = require('node:path')
+const dotenv = require('dotenv')
+dotenv.config()
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -32,25 +32,25 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildVoiceStates,
   ],
-});
-client.commands = new Collection();
+})
+client.commands = new Collection()
 
-const createChannels = [];
-const commandsPath = path.join(__dirname, "commands");
+const createChannels = []
+const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs
   .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+  .filter((file) => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
+  const filePath = path.join(commandsPath, file)
+  const command = require(filePath)
   // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command)
   } else {
     console.log(
       `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
+    )
   }
 }
 
@@ -58,35 +58,35 @@ const updateCreateChannels = async () => {
   const channel = await client.channels.fetch(
     serverInfo[0].createRoomCategory,
     {
-      type: "Category",
+      type: 'Category',
     }
-  );
+  )
   const textChannels = channel.children.cache.filter(
     (child) =>
-      child.id !== "1057885109367361647" && child.type === ChannelType.GuildText
-  );
+      child.id !== '1057885109367361647' && child.type === ChannelType.GuildText
+  )
 
   textChannels.forEach((tc) => {
-    const vcId = tc.topic;
-    const vc = channel.children.cache.find((child) => child.id === vcId);
-    createChannels.push({ vc, tc });
-  });
-  console.log("-----------------------");
+    const vcId = tc.topic
+    const vc = channel.children.cache.find((child) => child.id === vcId)
+    createChannels.push({ vc, tc })
+  })
+  console.log('-----------------------')
   createChannels.forEach((channel) => {
-    const { vc, tc } = channel;
+    const { vc, tc } = channel
     console.log({
       vc: { name: vc.name, id: vc.id },
       tc: { name: tc.name, id: tc.id },
-    });
-  });
-  console.log("-----------------------");
-};
+    })
+  })
+  console.log('-----------------------')
+}
 
-client.on("ready", async (client) => {
-  console.log(`==== Logged in: ${client.user.tag} ====`);
-  console.log("--------ready---------");
-  await updateCreateChannels();
-});
+client.on('ready', async (client) => {
+  console.log(`==== Logged in: ${client.user.tag} ====`)
+  console.log('--------ready---------')
+  await updateCreateChannels()
+})
 //メンションに「Hi!」と返信
 
 client.on('messageCreate', async (message) => {
@@ -94,7 +94,9 @@ client.on('messageCreate', async (message) => {
   if (message.mentions.users.has(client.user.id)) {
     message.reply('ぐまゆしです')
   }
-  const server = serverInfo.find((server) => server.guildId === message.guild.id)
+  const server = serverInfo.find(
+    (server) => server.guildId === message.guild.id
+  )
   if (!message.member) return
   if (message.system || message.member.user.bot === true) {
     console.log('システムまたはbotのメッセージ')
@@ -102,7 +104,7 @@ client.on('messageCreate', async (message) => {
   }
 
   const { channel } = message
-  if (message.content.startsWith("!dl") && message.guild) {
+  if (message.content.startsWith('!dl') && message.guild) {
     const messages = await channel.messages.fetch({ limit: 100 })
     const mentionMembers = await message.mentions.members.map((m) => m.user.id)
     const mentionFilter = await messages.filter((msg) =>
@@ -115,8 +117,8 @@ client.on('messageCreate', async (message) => {
     channel.bulkDelete(mentionFilter) // それらのメッセージを一括削除
   }
 
-  if (message.content === "9192631770") {
-    const members = await message.guild.members.fetch(); // メンバーを全員取得
+  if (message.content === '9192631770') {
+    const members = await message.guild.members.fetch() // メンバーを全員取得
     await members.map(async (member) => {
       await member.roles.add('1049170550758576248')
       await channel.send(`${member.displayName}に付与`)
@@ -154,21 +156,21 @@ client.on('messageCreate', async (message) => {
     if (serverInfo.some((server) => server.sleepText === message.channelId)) {
       const server = serverInfo.find(
         (server) => server.guildId === message.guild.id
-      );
-      const sleepText = message.guild.channels.cache.get(server.sleepText);
-      const memberChk = message.mentions.members.size !== 1;
-      const member = await message.mentions.members.first();
-      const sleepMember = await message.mentions.members.map((m) => m.user.id);
-      console.log(`${member}`);
+      )
+      const sleepText = message.guild.channels.cache.get(server.sleepText)
+      const memberChk = message.mentions.members.size !== 1
+      const member = await message.mentions.members.first()
+      const sleepMember = await message.mentions.members.map((m) => m.user.id)
+      console.log(`${member}`)
       switch (message.content) {
         case `<@${sleepMember}>`:
           if (!member.voice.channel) {
-            return sleepText.send("この人はVCに居ないよ？");
+            return sleepText.send('この人はVCに居ないよ？')
           }
-          const sleepChannel = message.guild.channels.cache.get(server.sleepVc);
+          const sleepChannel = message.guild.channels.cache.get(server.sleepVc)
 
-          await member.voice.setChannel(sleepChannel);
-          return sleepText.send(`${member}を寝落ち部屋に送ったよ！`);
+          await member.voice.setChannel(sleepChannel)
+          return sleepText.send(`${member}を寝落ち部屋に送ったよ！`)
       }
     }
   }
@@ -179,33 +181,33 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     if (
       createChannels.some((channel) => channel.vc.id === oldState.channelId)
     ) {
-      const newVCId = oldState.channel.id;
+      const newVCId = oldState.channel.id
       const isChangeChannel =
-        oldState.channelId !== undefined && newState.channelId !== undefined;
+        oldState.channelId !== undefined && newState.channelId !== undefined
       if (isChangeChannel) {
         const channelset = createChannels.find((channelset) => {
           if (channelset.vc) {
-            return channelset.vc.id === newVCId;
+            return channelset.vc.id === newVCId
           }
-        });
+        })
         if (channelset) {
           if (channelset.vc.members.size === 0) {
-            await channelset.vc.delete();
-            await channelset.tc.delete();
+            await channelset.vc.delete()
+            await channelset.tc.delete()
 
             createChannels.forEach((channels, i) => {
               if (!channels.vc) {
-                channels.tc.delete();
+                channels.tc.delete()
               } else if (!channels.tc) {
-                channels.vc.delete();
+                channels.vc.delete()
               }
-            });
-            await updateCreateChannels();
+            })
+            await updateCreateChannels()
           }
         } else {
           throw new Error(
             `channel move : channelset not found.  notFoundVCID: ${newVCId}`
-          );
+          )
         }
       }
     }
@@ -214,30 +216,30 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     const isLeaveChannel =
       oldState.channelId !== undefined && newState.channelId === undefined
     if (isLeaveChannel) {
-      const newVCId = oldState.channel.id;
+      const newVCId = oldState.channel.id
       const channelset = createChannels.find((channelset) => {
         if (channelset.vc) {
-          return channelset.vc.id === newVCId;
+          return channelset.vc.id === newVCId
         }
-      });
+      })
 
       if (channelset) {
         if (channelset.vc.members.size === 0) {
-          await channelset.vc.delete();
-          await channelset.tc.delete();
+          await channelset.vc.delete()
+          await channelset.tc.delete()
           createChannels.forEach((channels, i) => {
             if (!channels.vc) {
-              channels.tc.delete();
+              channels.tc.delete()
             } else if (!channels.tc) {
-              channels.vc.delete();
+              channels.vc.delete()
             }
-          });
-          await updateCreateChannels();
+          })
+          await updateCreateChannels()
         }
       } else {
         throw new Error(
           `channel leave : channelset not found. notFoundVCID: ${newVCId}`
-        );
+        )
       }
     }
   }
@@ -246,63 +248,62 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
 client.on('interactionCreate', async (interaction) => {
   const server = serverInfo.find(
     (server) => server.guildId === interaction.guild.id
-  );
+  )
 
   if (interaction.isChatInputCommand()) {
-    const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName)
 
     if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
-      return;
+      console.error(`No command matching ${interaction.commandName} was found.`)
+      return
     }
 
     try {
-      await command.execute(interaction);
+      await command.execute(interaction)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       await interaction.reply({
-        content: "There was an error while executing this command!",
+        content: 'There was an error while executing this command!',
         ephemeral: true,
-      });
+      })
     }
   }
 
-  if (interaction.customId === "ReNameModal") {
-    const modal = ReNameModalBuilder();
-    await interaction.showModal(modal); // 5
+  if (interaction.customId === 'ReNameModal') {
+    const modal = ReNameModalBuilder()
+    await interaction.showModal(modal) // 5
   }
-  if (interaction.customId === "ReNameModalFom") {
-    const TCId = interaction.channel.id;
+  if (interaction.customId === 'ReNameModalFom') {
+    const TCId = interaction.channel.id
 
     const channelset = createChannels.find((channelset) => {
-      return channelset.tc.id === TCId;
-    });
-    const value = interaction.fields.getTextInputValue("roomReName");
-    let newVc;
-    let newTc;
+      return channelset.tc.id === TCId
+    })
+    const value = interaction.fields.getTextInputValue('roomReName')
     try {
-      await createChannels.vc.setName(value);
-      await createChannels.tc.setName(value);
+      await channelset.vc.setName(value)
+      await channelset.tc.setName(value)
     } catch (er) {
-      console.log(er);
+      console.log(er)
     }
-    await updateCreateChannels();
+    await updateCreateChannels()
     interaction.reply({
-      content: "名前の変更をしました。",
+      content: '名前の変更をしました。',
       ephemeral: true,
-    });
-    return;
+    })
+    return
   }
   if (interaction.customId === 'memberLimitSet') {
     const modal = MemberLimitSetModalBuilder()
-    await interaction.showModal(modal) // 5
+    await interaction.showModal(modal)
   }
   if (interaction.customId === 'memberLimitSetFom') {
-    const nowVc = interaction.member.voice.channel
+    const TCId = interaction.channel.id
     const value = interaction.fields.getTextInputValue('setLimit')
-    await nowVc.setUserLimit(value)
+    const channelset = createChannels.find(
+      (channelset) => channelset.tc.id === TCId
+    )
+    await channelset.vc.setUserLimit(value)
     interaction.reply({
       content: '人数上限を変更しました。',
       ephemeral: true,
@@ -312,8 +313,8 @@ client.on('interactionCreate', async (interaction) => {
     const modal = SetRoomNameModalBuilder()
     await interaction.showModal(modal) // 5
   }
-  if (interaction.customId === "RoomButtonFom") {
-    const value = interaction.fields.getTextInputValue("roomName");
+  if (interaction.customId === 'RoomButtonFom') {
+    const value = interaction.fields.getTextInputValue('roomName')
     const newVc = await interaction.guild.channels.create({
       name: `${value}`,
       type: ChannelType.GuildVoice,
@@ -323,20 +324,20 @@ client.on('interactionCreate', async (interaction) => {
       name: `${value}`,
       type: ChannelType.GuildText,
       parent: server.createRoomCategory,
-    });
+    })
 
-    await newTc.setTopic(newVc.id);
+    await newTc.setTopic(newVc.id)
     interaction.reply({
       content: '生成しました',
       ephemeral: true,
-    });
-    createChannels.push({ vc: newVc, tc: newTc });
-    const roomSettingButtons = RoomSettingButtonsBuilder();
+    })
+    createChannels.push({ vc: newVc, tc: newTc })
+    const roomSettingButtons = RoomSettingButtonsBuilder()
     await showComponent(
       newTc,
       roomSettingButtons,
       `${interaction.member}\n以下のボタンから操作を選んでください\n部屋名の変更は10分間に一回のみなので、エラーが出た場合は10分後に再度お試し下さい`
-    );
+    )
   }
   if (interaction.customId === 'buildButton') {
     const championBuildSearchModal = ChampionBuildSearchModalBuilder()
@@ -377,10 +378,10 @@ client.on('interactionCreate', async (interaction) => {
   }
 })
 
-client.on("error", async (error) => {
-  const errorChannel = await client.channels.fetch("1059064447827722302");
-  console.error(error);
-  errorChannel.send(`Erorr: ${error.message}`);
-});
+client.on('error', async (error) => {
+  const errorChannel = await client.channels.fetch('1059064447827722302')
+  console.error(error)
+  errorChannel.send(`Erorr: ${error.message}`)
+})
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN)
